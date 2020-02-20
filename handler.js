48adee9d4d00500931f7c4065dc0a23abb75ef3e
milkywaynian <marina.faraji1@gmail.com>
2020-02-20 10:27:57 +0000

    initialised the query to get data

--- handler.js
+++ handler.js
@@ -3,8 +3,36 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const uuidv4 = require('uuid/v4');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host     : 'x',
+  user     : 'x',
+  password : 'x',
+  database : 'x'
+});
+
+
+
 
 app.get('/tasks', function(req, res) {
+
+  connection.query('SELECT * FROM `tasks` WHERE `userID` = "geoff"', function (error, results, fields) {
+    // error will be an Error if one occurred during the query
+    if(error) {
+      console.error('Your query had a problem with fetching tasks', error);
+      res.status(500);
+      res.json({errorMessage: error});
+    } else { 
+      res.json({tasks:results}); 
+
+    }
+    // results will contain the results of the query
+    // fields will contain information about the returned results fields (if any)
+  });
+
+
   res.json({
     message: 'Your API works',
   });
